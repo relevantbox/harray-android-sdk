092a1cfebec092706ec5894818cc84bb919353fa
Yildirim Adiguzel <yadiguzel@gmail.com>
2022-04-16 01:49:16 +0300

    version update

--- android-sdk/src/main/java/io/relevantbox/android/context/ApplicationContextHolder.java
+++ android-sdk/src/main/java/io/relevantbox/android/context/ApplicationContextHolder.java
@@ -12,7 +12,7 @@ import io.relevantbox.android.utils.RandomValueUtils;
 
 public class ApplicationContextHolder {
 
-    private final String sdkVersion = "3.2.6";
+    private final String sdkVersion = "3.2.7";
     private String persistentId;
     private boolean newInstallation = false;
 
